361b927dae42e2637273edbd0fd1616c0d3e9a7c
martin byrne <marb61a@gmail.com>
2017-10-13 11:55:02 +0000

    updated index and installed passport

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: 'auth/google/callback'
-}));
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('Access Token ', accessToken);    
+        console.log('Refresh Token ', refreshToken);
+        console.log('Profile', profile);
+        
+    }
+));
 
-app.listen(process.env.PORT, process.env.IP);
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }
+));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const port = process.env.PORT || 5000;
+app.listen(port);
