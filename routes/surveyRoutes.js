019f07720711646d6a66e792471203ff05bde99d
martin byrne <marb61a@gmail.com>
2017-11-29 17:03:06 +0000

    updated routed

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+const Survey = mongoose.model('surveys');
+
+module.exports = app => {
+    
+};
