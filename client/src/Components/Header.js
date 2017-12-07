52c3d8b149dc392f6081341b726f755cc673e4f6
martin byrne <marb61a@gmail.com>
2017-12-07 11:56:31 +0000

    adding Dashboard and header client components

---
+++ client/src/Components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
+
+class Header extends Component{
+    renderContent(){
+        
+    }
+    
+    render(){
+        return(
+            <nav>
+            
+            </nav>    
+        );
+    }
+}
