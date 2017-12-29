73ada2aa43d80d9bbcdb42d9be2b4022ee329da9
martin byrne <marb61a@gmail.com>
2017-12-29 18:00:31 +0000

    added SurveyFormReview file

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,27 @@
+// This file shows users their form inputs for review
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+    const reviewFields = _.map(formFields, ({ name, label }) => {
+        return(
+            <div key={name}>
+                <label>{label}</label>
+                <div>
+                    {formValues[name]}
+                </div>
+            </div>    
+        );    
+    }); 
+    
+    return(
+        <div>
+            <h5>Please confirm your entries</h5>
+            
+        </div>    
+    );
+};
--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -11,7 +11,21 @@ class SurveyList extends Component{
         return this.props.surveys.reverse().map(survey => {
             return(
                 <div className="card darken-1" key={survey._id}>
-                
+                    <div className="card-content">
+                        <span className="card-title">
+                            {survey.title}
+                        </span>
+                        <p>
+                            {survey.body}
+                        </p>
+                        <p className="right">
+                            Sent On: {new Date(survey.dateSent).toLocaleDateString()} 
+                        </p>
+                    </div>
+                    <div className="card-action">
+                        <a>Yes: {survey.yes}</a>
+                        <a>No: {survey.no}</a>
+                    </div>
                 </div>    
             );
         });
