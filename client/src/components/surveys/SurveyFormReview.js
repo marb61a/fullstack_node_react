57f7b09af7ff405075fb67f5daac5c9aa2c3731f
martin byrne <marb61a@gmail.com>
2018-01-23 18:00:32 +0000

    updated surveyformreview and models

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -22,7 +22,13 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
         <div>
             <h5>Please confirm your entries</h5>
             {reviewFields}
-            
+            <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
+                Back
+            </button>
+            <button onClick={() => submitSurvey(formValues, history)} className="green btn-flat right white-text">
+                Send Survey
+                <i className="material-icons right">email</i>
+            </button>
         </div>    
     );
 };
