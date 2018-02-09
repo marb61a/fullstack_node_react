9fdc380c9747cfa61bc70d421e78e450a84af3f5
martin byrne <marb61a@gmail.com>
2018-02-09 16:57:32 +0000

    updated routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -54,6 +54,34 @@ module.exports = app => {
         
         res.send({});
     });
+    
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+        const { title, subject, body, recipients } = req.body;
+        
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',')
+                .map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
+        
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        
+        try{
+            await mailer.send();
+            await survey.save();
+            req.user.credit -= 1;
+            
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err){
+            res.status(422).send(err);
+        }
+    });
+    
 };
 
 
